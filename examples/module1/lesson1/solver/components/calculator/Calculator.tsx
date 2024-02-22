d72fef558927f98172d0e740f178bee25c7faf71
modem1 <monika.dembek@knowit.eu>
2024-02-22 23:43:27 +0100

    Extract Calculator component

---
+++ examples/module1/lesson1/solver/components/calculator/Calculator.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { add, subtract, multiply, divide } from '../../utils/functions';
+
+function Calculator() {
+  const [firstNumber, setFirstNumber] = useState<number>(0);
+  const [secondNumber, setSecondNumber] = useState<number>(0);
+  const [result, setResult] = useState<number | string>(0);
+
+  const executeExpression = (func: (a: number, b: number) => number) => {
+    setResult(func(firstNumber, secondNumber));
+  };
+
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-x-4">
+        <input
+          type="number"
+          className="rounded-md shadow-md p-4"
+          value={firstNumber}
+          onChange={(e) => setFirstNumber(parseFloat(e.target.value))}
+        />
+        <input
+          type="number"
+          className="rounded-md shadow-md p-4"
+          value={secondNumber}
+          onChange={(e) => setSecondNumber(parseFloat(e.target.value))}
+        />
+      </div>
+      <div className="grid grid-cols-4 gap-x-4 my-4">
+        <button
+          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+          onClick={() => executeExpression(add)}
+        >
+          +
+        </button>
+        <button
+          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+          onClick={() => executeExpression(subtract)}
+        >
+          -
+        </button>
+        <button
+          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+          onClick={() => executeExpression(multiply)}
+        >
+          *
+        </button>
+        <button
+          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+          onClick={() => executeExpression(divide)}
+        >
+          /
+        </button>
+      </div>
+      <div>Result: {result}</div>
+    </div>
+  );
+}
+
+export default Calculator;
